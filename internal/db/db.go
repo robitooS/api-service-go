22e22b6c2a270b85a5390ae583263a71bac1c9e9
Higor de Souza Robin <proghigorrobin@gmail.com>
2025-09-18 17:35:06 -0300

    feat: add arquivo de config do db

---
+++ internal/db/db.go
@@ -0,0 +1,45 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/robitooS/api-service-go/internal/config"
+)
+
+//! A dsn será passado pela main
+func Connect(cfg *config.Config) (*sql.DB, error) {
+	var err error
+
+	pool, err := sql.Open("sqlite3", cfg.DBPath)
+	if err != nil {
+		// Aqui não é um erro de conexão, pode ser erro sobre o dsn ou outros 
+		return nil, fmt.Errorf("não foi possível utilizar o dsn: %v", err)
+	}
+
+	// Seta limite de conexões do pool, tempo de vida da conn e qtd de conn ociosas guardadas 
+	pool.SetConnMaxLifetime(0)
+	pool.SetMaxIdleConns(3)
+	pool.SetMaxOpenConns(3)
+
+	// Validar a conexão do db com ping
+	err = ping(context.Background(), pool)
+	if err != nil {
+		pool.Close() // Fechar o pool de conexões caso o ping falhe
+		return nil, err
+	}
+
+	return pool, nil
+}
+
+func ping(ctx context.Context, pool *sql.DB) error {
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+
+	if err := pool.PingContext(ctx); err != nil {
+		return fmt.Errorf("não foi possível efetuar uma conexão com o db: %v", err)
+	}
+	return nil
+}
