9091e9051ccf6924792edc6949879704162cf0e0
Higor de Souza Robin <proghigorrobin@gmail.com>
2025-09-23 11:10:17 -0300

    feat: migrations - adiciona tabela users

---
+++ internal/migrations/001.init_users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_name TEXT NOT NULL,
+    user_email TEXT NOT NULL UNIQUE,
+    user_password TEXT NOT NULL,
+    user_created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP 
+)
