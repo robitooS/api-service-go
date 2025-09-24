47738219978ab077f7b0ebd1fce9db8154af2112
Higor Robin <proghigorrobin@gmail.com>
2025-09-23 23:36:37 -0300

    feat: criado construtor com validações básicas para criação de um user

--- internal/domain/user/user.go
+++ internal/domain/user/user.go
@@ -1,10 +1,29 @@
 package user
 
-import "time"
+import (
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
-	ID        string
 	Name      string
 	Email     string
-	CreatedAt time.Time
 }
+
+func NewUser(name, email, password string) (*User, *Credentials, error) {
+	if err := ValidateName(name); err != nil {
+		return nil, nil, err
+	}
+	if err := ValidateEmail(email); err != nil {
+		return nil, nil, err
+	}
+	if err := ValidatePassword(password); err != nil {
+		return nil, nil, err
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return &User{Name: name, Email: email}, &Credentials{PasswordHash: string(hash)}, nil
+}	
