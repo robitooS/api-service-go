1901e0b2dd84c23d975ab3e23d1db3e95bc568ed
Higor de Souza Robin <proghigorrobin@gmail.com>
2025-09-30 17:25:43 -0300

    feat: add nova rota para usuario com auth e funções de busca e login

--- internal/routes/user_route.go
+++ internal/routes/user_route.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 
 	"github.com/gin-gonic/gin"
+	"github.com/robitooS/api-service-go/internal/auth"
 	"github.com/robitooS/api-service-go/internal/config"
 	"github.com/robitooS/api-service-go/internal/handlers"
 	"github.com/robitooS/api-service-go/internal/repository"
@@ -13,22 +14,21 @@ import (
 func UserRoutes(router *gin.Engine, pool *sql.DB, cfg *config.Config) {
 	// primeiro deve injetar as dependências 
 	userRepository := repository.NewUserRepository(pool)
-	userService := service.NewUserService(userRepository)
+	userService := service.NewUserService(userRepository, cfg.HmacSecret)
 	userHandler := handlers.NewUserHandler(userService) // handler responsável pelo usuário
 
 	// Rotas públicas
 	publicUsersRoutes := router.Group("/users")
 	{
-		publicUsersRoutes.POST("list") // listar todos os usuários
 		publicUsersRoutes.POST("/create", userHandler.CreateUser)
-		publicUsersRoutes.POST("/get") // buscar por id especifico
+		publicUsersRoutes.POST("/login", userHandler.Login)
 		
 		// daq p frente será adicionado mais rotas
 	}
 
-	// // Rotas protegidas (HMAC)
-	// authUsersRoutes := router.Group("/users", auth.AuthenticateHMAC([]byte(cfg.HmacSecret), userRepository))
-	// {
-
-	// }
+	// Rotas protegidas (HMAC)
+	authUsersRoutes := router.Group("/users", auth.AuthenticateHMAC([]byte(cfg.HmacSecret), userRepository))
+	{
+		authUsersRoutes.POST("/get", userHandler.GetUserByID)
+	}
 }
--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -2,16 +2,24 @@ package service
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"github.com/robitooS/api-service-go/internal/auth"
 	"github.com/robitooS/api-service-go/internal/domain/user"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService struct {
 	UserRepository user.UserRepository
+	HmacSecret []byte
 }
 
-func NewUserService(userRepository user.UserRepository) *UserService {
-	return &UserService{UserRepository: userRepository}
+func NewUserService(userRepository user.UserRepository, hmacSecret string) *UserService {
+	return &UserService{
+		UserRepository: userRepository,
+		HmacSecret: []byte(hmacSecret),
+	}
 }
 
 func (s *UserService) Create (ctx context.Context, name string, email string, password string) (*user.User, error){
@@ -29,4 +37,43 @@ func (s *UserService) Create (ctx context.Context, name string, email string, pa
 	}
 
 	return createdUser, nil
+}
+
+// Struct de resposta após login
+type AuthResponse struct {
+	UserID int64 `json:"user_id"`
+	Timestamp int64 `json:"timestamp"`
+	Signature string `json:"signature"`
+}
+
+func (s *UserService) Login (ctx context.Context, email string, password string) (*AuthResponse, error) {
+	// Verifica primeiro se o user existe
+	user, creds, err := s.UserRepository.FindByEmail(ctx, email)
+	if err != nil {
+		return nil, fmt.Errorf("usuário não encontrado")
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(creds.PasswordHash), []byte(password)); err != nil {
+		return nil, fmt.Errorf("credenciais inválidas")
+	}
+
+	timestamp := time.Now().Unix()
+	msg := auth.BuildMessage(user.ID, timestamp)
+	signature := auth.GenerateSignature(msg, s.HmacSecret)
+
+	resp := AuthResponse{
+		UserID: user.ID,
+		Timestamp: timestamp,
+		Signature: signature,
+	}
+
+	return &resp, nil
+}
+
+func (s *UserService) GetByID(ctx context.Context, id int64) (*user.User, error) {
+	user, err := s.UserRepository.FindByID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
