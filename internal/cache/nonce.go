dbc5002e820979d756700a2501d3453ec079878e
Higor de Souza Robin <proghigorrobin@gmail.com>
2025-10-01 17:38:10 -0300

    feat: add simulação de cache para os nonce, usado para unicidade em cada requisição

---
+++ internal/cache/nonce.go
@@ -0,0 +1,59 @@
+package cache
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type NonceStore interface {
+	CacheNonce(nonce string) error
+	runCleaner()
+}
+
+type InMemoryNonceStore struct {
+	Storage map[string]time.Time
+	mu		sync.RWMutex
+}
+
+// CacheNonce implements NonceStore.
+func (i *InMemoryNonceStore) CacheNonce(nonce string) error {
+	// evitar concorrencia de escrita
+	i.mu.Lock()
+	defer i.mu.Unlock()
+
+	if _, ok := i.Storage[nonce]; ok {
+		return fmt.Errorf("o nonce já foi usado")
+
+	}
+	i.Storage[nonce] = time.Now()
+	return nil
+}
+
+func NewInMemoryNonceStore() NonceStore {
+	// Cria o objeto
+	inMemoryNonceStore := &InMemoryNonceStore{Storage: make(map[string]time.Time)}
+
+	go inMemoryNonceStore.runCleaner()
+
+	return inMemoryNonceStore
+}
+
+func (i *InMemoryNonceStore) runCleaner() {
+    ticker := time.NewTicker(5 * time.Minute)
+    defer ticker.Stop()
+
+    for range ticker.C {
+        i.mu.Lock()
+
+		// Ficar verificando se já expirou os 5 minutos
+		// Vamo ficar vendo o valor de cada nonce e vendo se já passou 5 minutos
+		// time desde o tempo colocado no map é maior q 5 minutos?
+        for nonce, timestamp := range i.Storage {
+            if time.Since(timestamp) > 5*time.Minute {
+                delete(i.Storage, nonce)
+            }
+        }
+        i.mu.Unlock()
+    }
+}
